d8138a831cc372bed9c49641b6194e19614b88eb
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2025-12-08 20:26:37 -0500

    remove unused scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,30 @@
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+import 'animate.css/animate.min.css'
 import Swal from 'sweetalert2'
 
-function aplicarAnimacion(id, anim) {
+const ANIMATION_DURATION = '5s'
+const ANIMATION_TIMEOUT = 1000
+
+function aplicarAnimacion(
+	id,
+	anim,
+	duration = ANIMATION_DURATION,
+	timeout = ANIMATION_TIMEOUT
+) {
 	const el = $('#' + id)
+	el.css('animation-duration', duration)
 	el.removeClass('animate__animated animate__' + anim)
 	void el[0].offsetWidth
 	el.addClass('animate__animated animate__' + anim)
 
-	el.one('animationend', function () {
+	setTimeout(() => {
 		Swal.fire('Animación aplicada: ' + anim).then(() => {
 			el.removeClass('animate__animated animate__' + anim)
+			el.css('animation-duration', '')
 		})
-	})
+	}, timeout)
 }
 
 $('img').each(function () {
